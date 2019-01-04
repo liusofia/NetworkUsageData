1cfae53229ff3ea956da92d74bb56ca03424458e
liuyixi <liuyixi@xiaomi.com>
2019-01-04 16:24:50 +0800

    sofia:update total summary.

--- NetworkUsageData/java/data/usge/network/wifi/com/networkusagedata/NetworkUsageActivity.java
+++ NetworkUsageData/java/data/usge/network/wifi/com/networkusagedata/NetworkUsageActivity.java
@@ -5,83 +5,127 @@ import android.app.Activity;
 import android.app.usage.NetworkStats;
 import android.app.usage.NetworkStatsManager;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.net.ConnectivityManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.HashMap;
-
+import java.util.List;
+import java.util.Map;
 
 public class NetworkUsageActivity extends Activity {
 
-    private static final String TAG = "NetworkUsageActivity";
+    private static final String TAG = "liuyixi0103";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network_usage);
+        TextView showDataUssageStatsTitle = findViewById(R.id.data_usage_stats_title);
+        showDataUssageStatsTitle.setVisibility(View.VISIBLE);
 
         NetworkStatsManager networkStatsManager = (NetworkStatsManager) getSystemService(Context.NETWORK_STATS_SERVICE);
         //获取总流量
-        getSummaryForDevice(networkStatsManager);
-
+        computeTotalUsageDataForDevice(networkStatsManager,getDurationTime());
         //获取应用流量
-        HashMap<Integer, Long> dcMap = getDcforAllUids(networkStatsManager, getDurationTime());
+        HashMap<Integer, Long> dcMap = computeDataUsageforAllUids(networkStatsManager, getDurationTime());
+        //获取应用的uid
+        getApplicationUids();
     }
 
-    public void getSummaryForDevice(NetworkStatsManager networkStatsManager){
+    @TargetApi(Build.VERSION_CODES.O)
+    public void computeTotalUsageDataForDevice(NetworkStatsManager networkStatsManager, Duration duration){
         NetworkStats.Bucket bucket = null;
+        long result = 0L;
         try{
-            bucket = networkStatsManager.querySummaryForDevice(ConnectivityManager.TYPE_WIFI, "", 0, System.currentTimeMillis());
-            Log.d(TAG, "Total: " + (bucket.getRxBytes() + bucket.getTxBytes()));
+            int[] types = new int[]{
+                    ConnectivityManager.TYPE_WIFI, ConnectivityManager.TYPE_ETHERNET
+            };
+
+            for(int i = 0;i < types.length;i++){
+                //结果是整个设备的汇总数据使用情况
+                bucket = networkStatsManager.querySummaryForDevice(types[i], "", duration.start, duration.end);
+                if (bucket != null) {
+                    long resultTmp = bucket.getRxBytes() + bucket.getTxBytes();
+                    result += resultTmp;
+                }
+            }
         }catch (Exception e){
             e.printStackTrace();
+        }
+        String total = String.format(getResources().getString(R.string.data_usage_total_summay_text), Formatter.formatFileSize(getApplicationContext(),result));
+        TextView showTotalDataUsage = ((TextView)findViewById(R.id.data_usage_total_summay));
+        showTotalDataUsage.setText(total);
+        showTotalDataUsage.setVisibility(View.VISIBLE);
+        Log.d(TAG, "Total: " + total);
+    }
+
+    //获取application 的 packageName/processName
+    public void getApplicationUids(){
+        //内核分配给application 的内核用户标识
+        Map<Integer, ApplicationInfo> uids = new HashMap<Integer, ApplicationInfo>();
+        //获取设备上安装应用的列表
+        List<ApplicationInfo> applications = getPackageManager().getInstalledApplications(0);
+        for (int i = 0; i < applications.size(); i++) {
+            int uid = applications.get(i).uid;
+            uids.put(uid, applications.get(i));
+            Log.d(TAG,"processName = " + applications.get(i).processName + "  / uid = " + uid);
         }
     }
 
     @TargetApi(Build.VERSION_CODES.O)
-    private static HashMap<Integer, Long> getDcforAllUids(NetworkStatsManager networkStatsManager, Duration duration) {
+    //根据UID 查询网络使用情况
+    private static HashMap<Integer, Long> computeDataUsageforAllUids(NetworkStatsManager networkStatsManager, Duration duration) {
         NetworkStats stats = null;
         HashMap<Integer, Long> map = new HashMap<Integer, Long>();
         int bucketUid = -1;
-        long rt = 0L;
-        int count = 0;
+        long result = 0L;
+        int count;
         try {
-            int[] types = new int[]{ ConnectivityManager.TYPE_WIFI, ConnectivityManager.TYPE_ETHERNET };
+            //参数1:定义在ContivityManager中的网络接口类型
+            //参数2:网络接口的用户标识
+            int[] types = new int[]{
+                    ConnectivityManager.TYPE_WIFI, ConnectivityManager.TYPE_ETHERNET
+            };
+
             for (int i = 0; i < types.length; i++) {
                 stats = networkStatsManager.querySummary(types[i], "", duration.start, duration.end);
                 if (stats != null) {
                     count = 0;
                     do {
                         NetworkStats.Bucket bucket = new NetworkStats.Bucket();
-                        boolean bl = stats.getNextBucket(bucket);
-                        if (bl == true) {
-                            rt = bucket.getRxBytes() + bucket.getTxBytes();
+                        boolean success = stats.getNextBucket(bucket);
+                        if (success == true) {
+                            result = bucket.getRxBytes() + bucket.getTxBytes();
                             bucketUid = bucket.getUid();
+                            //app uid 或 通过网络共享使用数据的uid 或 系统uid
                             long last = (map.get(bucketUid) == null ? 0L : map.get(bucketUid));
-                            map.put(bucketUid, last + rt);
+                            map.put(bucketUid, last + result);
                         }
-                        Log.d(TAG,"getDcforAllUids() into , count = " + count + ", bl = " + bl + ", bucketUid = " + bucketUid + ", rt = " + rt);
+                        Log.d(TAG,"computeDataUsageforAllUids() into , count = " + count + ", success = " + success + ", bucketUid = " + bucketUid + ", result = " + result);
                         count++;
-
                     } while (stats.hasNextBucket());
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Log.i(TAG, "================> getDcforAllUids, map.size = " + map.size());
+        Log.d(TAG, "computeDataUsageforAllUids(), map.size = " + map.size());
 
         // dev
         for (Integer key : map.keySet()) {
-            Log.i(TAG, "=====================> buckt-map(" + key + ") = " + map.get(key));
+            Log.d(TAG, "buckt-map(" + key + ") = " + map.get(key));
         }
-
         return map;
     }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //用于获取起始时间
     public Duration getDurationTime(){
         //用于获得日期
