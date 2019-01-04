843503eea2fe70e3679a2a088657b455363fd648
liuyixi <liuyixi@xiaomi.com>
2019-01-04 11:19:51 +0800

    sofia:remove unused code.

--- NetworkUsageData/java/data/usge/network/wifi/com/networkusagedata/NetworkUsageActivity.java
+++ NetworkUsageData/java/data/usge/network/wifi/com/networkusagedata/NetworkUsageActivity.java
@@ -2,15 +2,11 @@ package data.usge.network.wifi.com.networkusagedata;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.AppOpsManager;
 import android.app.usage.NetworkStats;
 import android.app.usage.NetworkStatsManager;
 import android.content.Context;
-import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Build;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -26,11 +22,6 @@ public class NetworkUsageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network_usage);
-        //获取包名的uid
-//        getUidByPackageName();
-
-        //动态获取权限
-//        hasPermissionToReadNetworkStats();
 
         NetworkStatsManager networkStatsManager = (NetworkStatsManager) getSystemService(Context.NETWORK_STATS_SERVICE);
         //获取总流量
@@ -38,8 +29,6 @@ public class NetworkUsageActivity extends Activity {
 
         //获取应用流量
         HashMap<Integer, Long> dcMap = getDcforAllUids(networkStatsManager, getDurationTime());
-
-//        computeTotalConsumption(networkStatsManager,getDurationTime());
     }
 
     public void getSummaryForDevice(NetworkStatsManager networkStatsManager){
@@ -52,29 +41,8 @@ public class NetworkUsageActivity extends Activity {
         }
     }
 
-    private boolean hasPermissionToReadNetworkStats() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        final AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
-        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
-                android.os.Process.myUid(), getPackageName());
-        if (mode == AppOpsManager.MODE_ALLOWED) {
-            return true;
-        }
-
-        requestReadNetworkStats();
-        return false;
-    }
-    // 打开“有权查看使用情况的应用”页面
-    private void requestReadNetworkStats() {
-        Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
-        startActivity(intent);
-    }
-
     @TargetApi(Build.VERSION_CODES.O)
     private static HashMap<Integer, Long> getDcforAllUids(NetworkStatsManager networkStatsManager, Duration duration) {
-        Log.d(TAG,"getDcforAllUids into");
         NetworkStats stats = null;
         HashMap<Integer, Long> map = new HashMap<Integer, Long>();
         int bucketUid = -1;
@@ -83,7 +51,7 @@ public class NetworkUsageActivity extends Activity {
         try {
             int[] types = new int[]{ ConnectivityManager.TYPE_WIFI, ConnectivityManager.TYPE_ETHERNET };
             for (int i = 0; i < types.length; i++) {
-                stats = networkStatsManager.querySummary(ConnectivityManager.TYPE_WIFI, "", getTimesMonthMorning(), System.currentTimeMillis());
+                stats = networkStatsManager.querySummary(types[i], "", duration.start, duration.end);
                 if (stats != null) {
                     count = 0;
                     do {
@@ -113,46 +81,6 @@ public class NetworkUsageActivity extends Activity {
 
         return map;
     }
-
-    public static long getTimesMonthMorning() {
-        Calendar cal = Calendar.getInstance();
-        cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
-        cal.set(Calendar.DAY_OF_MONTH, cal.getActualMinimum(Calendar.DAY_OF_MONTH));
-        return cal.getTimeInMillis();
-    }
-
-//    //查询设备总的流量统计信息
-//    private static long computeTotalConsumption(NetworkStatsManager networkStatsManager, Duration duration) {
-//        long rt = 0L;
-//        try {
-//            int[] types = new int[]{ ConnectivityManager.TYPE_WIFI, ConnectivityManager.TYPE_ETHERNET };
-//            for (int i = 0; i < types.length; i++) {
-//                //第二个参数为网络接口标示
-//                NetworkStats.Bucket bucket = networkStatsManager.querySummaryForDevice(types[i], null, duration.start, duration.end);
-//                if (bucket != null) {
-//                    long rtTmp = bucket.getRxBytes() + bucket.getTxBytes();
-//                    rt += rtTmp;
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        Log.i(TAG, "================> computeTotalConsumption, rt = " + rt);
-//        return rt;
-//    }
-
-//    public static int getUidByPackageName(Context context, String packageName) {
-//        int packageUid = -1;
-//        PackageManager packageManager = context.getPackageManager();
-//        try{
-//            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_META_DATA);
-//            packageUid = packageInfo.applicationInfo.uid;
-//            Log.d("liuyixi", packageInfo.packageName + " uid: " + uid);
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        return packageUid;
-//    }
 
     //用于获取起始时间
     public Duration getDurationTime(){
